bd5610017a5d48cd294821f7115d70256a215e83
CodySingh <arjan.singh1121@gmail.com>
2025-09-21 21:56:44 +0300

    i think im done
    not gonna make the changes, I did not rreally like this project not gonna lie. I rate this project a 2/10

--- script.js
+++ script.js
@@ -9,5 +9,33 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
      }
+     inputBox.value = '';
+     saveData();
 }
+
+listContainer.addEventListener("click", function(clicked){
+    if(clicked.target.tagName === "LI"){
+        clicked.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(clicked.target.tagName === "SPAN"){
+        clicked.target.parentElement.remove();
+        saveData();
+    }
+
+    
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
